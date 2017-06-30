443e99356166256c9f51147f001e0764d949a5c9
Druotic <jamesjbeavers@gmail.com>
2017-06-29 20:15:54 -0400

    [WB-20] Create collection helper file, pull in specific lodash functions

---
+++ util/objectHelpers.js
@@ -0,0 +1,8 @@
+/*
+ * Helper functions for dealing with objects.
+ */
+
+exports.get = require('lodash.get')
+exports.set = require('lodash.set')
+exports.union = require('lodash.union')
+exports.keys = require('lodash.keys')
