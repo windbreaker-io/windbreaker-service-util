d1af451637086f09402f10a1b9979c31ec8719dd
Austin Kelleher <austin.kell47@gmail.com>
2017-07-08 14:04:01 -0400

    [NO-ISSUE] Rearrange tests to be more like source structure. (#16)

--- test/unit/dao/Entity-test.js
+++ test/unit/dao/Entity-test.js
@@ -15,15 +15,3 @@ test('should allow passing id', (t) => {
   const entity = new Entity({ id: id })
   t.is(entity.getId(), id)
 })
-
-test('should allow fetching id using getId', (t) => {
-  const entity = new Entity()
-  t.true(assertUuid(entity.getId()))
-})
-
-test('should allow setting id using setId', (t) => {
-  const entity = new Entity()
-  const id = uuid.v4()
-  entity.setId(id)
-  t.is(entity.getId(), id)
-})
