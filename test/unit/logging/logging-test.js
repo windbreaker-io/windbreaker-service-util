9649132c259e67e3b87d654cd009d41193963766
austinkelleher <austin.kell47@gmail.com>
2017-07-08 15:04:02 -0400

    [NO-ISSUE] Improve test coverage of logging module.

--- test/unit/logging/logging-test.js
+++ test/unit/logging/logging-test.js
@@ -4,6 +4,7 @@ const { test } = require('ava')
 const sinon = require('sinon')
 const bunyan = require('bunyan')
 const logging = require('~/logging')
+const LogLevel = require('~/models/LogLevel')
 
 class MockLogger {
   constructor (options) {
@@ -16,6 +17,12 @@ class MockLogger {
     this.name = name
     this.level = level
     this.streams = streams
+    this.fileName = undefined
+  }
+
+  child ({ fileName }) {
+    this.fileName = fileName
+    return this
   }
 }
 
@@ -46,4 +53,73 @@ test('should allow configuring logger', (t) => {
   t.is(logger.level, 'info')
   t.is(logging.getProjectRootPath(),
     packagePath.substring(0, packagePath.length - 'package.json'.length))
+})
+
+test('should have default logger name if none passed', (t) => {
+  const packagePath = require.resolve('~/package.json')
+
+  let logger = logging.configure({
+    packagePath,
+    logLevel: 'info'
+  })
+
+  t.is(logger.name, 'windbreaker')
+  t.is(logger.level, 'info')
+  t.is(logging.getProjectRootPath(),
+    packagePath.substring(0, packagePath.length - 'package.json'.length))
+})
+
+test('should allow passing logLevel enum model', (t) => {
+  const packagePath = require.resolve('~/package.json')
+
+  let logger = logging.configure({
+    packagePath,
+    logLevel: LogLevel.INFO,
+    loggerName: 'test-logger'
+  })
+
+  t.is(logger.name, 'test-logger')
+  t.is(logger.level, 'info')
+  t.is(logging.getProjectRootPath(),
+    packagePath.substring(0, packagePath.length - 'package.json'.length))
+})
+
+test('should give a logger instance calling "logger" with a module passed', (t) => {
+  const packagePath = require.resolve('~/package.json')
+
+  logging.configure({
+    packagePath,
+    logLevel: LogLevel.INFO,
+    loggerName: 'test-logger'
+  })
+
+  const logger = logging.logger(module)
+  const rootPath = logging.getProjectRootPath()
+
+  t.is(logger.name, 'test-logger')
+  t.is(logger.level, 'info')
+  t.is(rootPath,
+    packagePath.substring(0, packagePath.length - 'package.json'.length))
+  t.is(logger.fileName,
+    module.filename.substring(rootPath.length))
+})
+
+test('should give a logger instance calling "logger" without a module passed', (t) => {
+  const packagePath = require.resolve('~/package.json')
+
+  logging.configure({
+    packagePath,
+    logLevel: LogLevel.INFO,
+    loggerName: 'test-logger'
+  })
+
+  const logger = logging.logger()
+  const rootPath = logging.getProjectRootPath()
+
+  t.is(logger.name, 'test-logger')
+  t.is(logger.level, 'info')
+  t.is(rootPath,
+    packagePath.substring(0, packagePath.length - 'package.json'.length))
+  t.is(logger.fileName,
+    module.filename.substring(rootPath.length))
 })
