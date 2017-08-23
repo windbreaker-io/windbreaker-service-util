fd821cd7e1ad1e5ac85ede2d88e444b9a0a565df
Austin Kelleher <austin.kell47@gmail.com>
2017-08-23 19:48:09 -0400

    [WB-51] Add support for non-strict Event wrappping during Event convertData. (#24)

--- models/events/webhook/github/GithubPush.js
+++ models/events/webhook/github/GithubPush.js
@@ -1,5 +1,21 @@
+const GithubCommit = require('./GithubCommit')
+
 module.exports = require('../../../Model').extend({
+  typeName: 'github-push',
+  strict: false,
   properties: {
-    compare: String
+    ref: String,
+    before: String,
+    after: String,
+    created: Boolean,
+    deleted: Boolean,
+    forced: Boolean,
+    base_ref: String,
+    compare: String,
+    commits: [GithubCommit],
+    head_commit: Object,
+    repository: Object,
+    pusher: Object,
+    sender: Object
   }
 })
--- test/integration/queue/event-convert-data-test.js
+++ test/integration/queue/event-convert-data-test.js
@@ -15,3 +15,20 @@ test('should convert data for a valid EventType', (t) => {
 
   t.is(event.getData().getCompare(), 'abc123')
 })
+
+test('should not throw wrap errors for model that has strict type false', (t) => {
+  const event = new Event({
+    // GithubPush is a model that has `strict` set to `false`
+    type: 'github-push',
+    data: {
+      compare: 'abc123',
+      randomProp: 'fail'
+    }
+  })
+
+  let errors = []
+  event.convertData(errors)
+
+  t.is(errors.length, 0)
+  t.is(event.getData().getCompare(), 'abc123')
+})
