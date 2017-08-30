068a9161898edbdb97319c6858050b8de1814af1
austinkelleher <austin.kell47@gmail.com>
2017-08-30 19:25:08 -0400

    [WB-55] Allow loading config synchronously.

--- config/index.js
+++ config/index.js
@@ -15,6 +15,28 @@ function printConfig (config) {
   console.log('\n')
 }
 
+function applyOverrides (config, allOverrides, overrides) {
+  if (overrides) {
+    Object.assign(allOverrides, ...overrides)
+  }
+
+  for (const key in allOverrides) {
+    // TODO: Consider checking if config.get(key) is undefined
+    config.set(key, allOverrides[key])
+  }
+
+  printConfig(config)
+  // force config to be immutable
+  Object.freeze(config)
+  return config
+}
+
+function getOverrideFilePath (overridePath) {
+  const serviceEnv = BaseServiceConfig.getServiceEnvironment()
+  const env = (serviceEnv && serviceEnv.toLowerCase()) || 'localhost'
+  return path.join(overridePath, `${env}.yml`)
+}
+
 /**
 * Service environment env variable must be set to load non-default config file.
 *
@@ -42,21 +64,41 @@ exports.load = async function load ({ config, path: overridePath, overrides }) {
 
   let allOverrides = {}
   if (overridePath) {
-    const serviceEnv = BaseServiceConfig.getServiceEnvironment()
-    const env = (serviceEnv && serviceEnv.toLowerCase()) || 'localhost'
-    allOverrides = await confugu.load(path.join(overridePath, `${env}.yml`))
+    allOverrides = await confugu.load(getOverrideFilePath(overridePath))
   }
 
-  if (overrides) {
-    Object.assign(allOverrides, ...overrides)
+  return applyOverrides(config, allOverrides, overrides)
+}
+
+/**
+* Service environment env variable must be set to load non-default config file.
+*
+* @param obj.config {Config} - Instance of the `Config` model
+* @param obj.path {string} - Absolute path to config directory
+* @param obj.overrides {Object[]} - Array of objects where the keys are the `Config`
+*    model property keys and the values are the corresponding values to that key.
+*    Useful for passing the values of files and values from process.argv
+*
+*    e.g.
+*
+*    [
+*      { environment: 'PRODUCTION' }
+*      { logLevel: 'debug' },
+*    ]
+*
+*/
+exports.loadSync = function loadSync ({ config, path: overridePath, overrides }) {
+  assert(!overrides || Array.isArray(overrides),
+    'if present, overrides must be an array')
+
+  if (config.applyDefaults) {
+    config.applyDefaults()
   }
 
-  for (const key in allOverrides) {
-    config.set(key, allOverrides[key])
+  let allOverrides = {}
+  if (overridePath) {
+    allOverrides = confugu.loadSync(getOverrideFilePath(overridePath))
   }
 
-  printConfig(config)
-  // force config to be immutable
-  Object.freeze(config)
-  return config
+  return applyOverrides(config, allOverrides, overrides)
 }
