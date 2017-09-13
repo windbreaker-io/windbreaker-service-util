792b3643c82d24c8cf3af86a9e10ca2025c62cff
Mabry Cervin <mabry.cervin@gmail.com>
2017-09-13 18:08:07 -0400

    Removed mock changes stream

--- test/mocks/MockChangesStream.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events')
-const Promise = require('bluebird')
-
-module.exports = class MockChangesStream extends EventEmitter {
-  destroy () {
-    return Promise.resolve()
-  }
-}
